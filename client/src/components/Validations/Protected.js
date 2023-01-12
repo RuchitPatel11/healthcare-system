35974269b4e6079f8a2ee14aa384619baf1b1301
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2023-01-12 10:19:07 +0530

    Jan12

--- client/src/components/Validations/Protected.js
+++
@@ -1,6 +0,0 @@
-// import { Navigate } from 'react-router-dom';
-
-// function Protected({ condition, children, redirectTo = '/' }) {
-//   return condition ? { ...children } : <Navigate to={redirectTo} />;
-// }
-// export default Protected;
