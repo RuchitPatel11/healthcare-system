331cee006dca770c0125c6713c687948b8832eb0
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2023-01-13 17:55:50 +0530

    Jan13

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -63,64 +63,73 @@ const Login = () => {
           className="absolute top-0 left-0 object-fill w-full h-full -z-10"
           alt="login-bg"
         />
-        <div className="flex gap-16 bg-white shadow-xl p-28 ">
-          <div>
-            <img
-              src="images/login-bg.png"
-              alt="login-bg.png"
-              className="transition duration-200 shadow-xl hover:scale-110"
-            />
+        <div className="flex flex-col gap-16 p-24 bg-white shadow-xl">
+          <div className="flex justify-center">
+            <p className="text-3xl font-bold underline text-secondary decoration-4 underline-offset-8 decoration-primary">
+              Sign In
+            </p>
           </div>
-          <div className="flex flex-col gap-10">
-            <div className="flex flex-col gap-3">
-              <h1 className="flex gap-1 mb-3 font-semibold">
-                New User?
-                <Link to="/register">
-                  <p className="font-medium text-blue-600 text-md">Sign Up</p>
-                </Link>
-              </h1>
-              <h1 className="text-4xl font-extrabold text-purple">
-                Welcome Back!
-              </h1>
-              <h1 className="text-lg text-mute">Login to continue</h1>
-            </div>
+          <div className="flex gap-12">
             <div>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col gap-3">
-                  <FormField
-                    type="text"
-                    placeholder="you@gmail.com"
-                    error={errors.email}
-                    register={register("email")}
-                    name="email"
-                    icon="fa-solid fa-envelope"
-                  />
-                  <FormField
-                    type="password"
-                    placeholder="Enter Password"
-                    error={errors.password}
-                    register={register("password")}
-                    name="password"
-                    icon="fa-solid fa-lock"
-                  />
+              <img
+                src="images/login-bg.png"
+                alt="login-bg.png"
+                className="transition duration-200 shadow-xl hover:scale-110"
+              />
+            </div>
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col gap-3">
+                <h1 className="flex gap-1 mb-3 font-semibold">
+                  New User?
+                  <Link to="/register">
+                    <p className="font-medium text-blue-600 text-md">Sign Up</p>
+                  </Link>
+                </h1>
+                <h1 className="text-4xl font-extrabold text-purple">
+                  Welcome Back!
+                </h1>
+                <h1 className="text-lg text-mute">Login to continue</h1>
+              </div>
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="flex flex-col gap-3">
+                    <FormField
+                      type="text"
+                      placeholder="you@gmail.com"
+                      error={errors.email}
+                      register={register("email")}
+                      name="email"
+                      icon="fa-solid fa-envelope"
+                    />
+                    <FormField
+                      type="password"
+                      placeholder="Enter Password"
+                      error={errors.password}
+                      register={register("password")}
+                      name="password"
+                      icon="fa-solid fa-lock"
+                    />
 
-                  <div className="flex items-center gap-10">
-                    <div>
-                      <button
-                        type="submit"
-                        className="px-8 py-2.5 rounded-full bg-primary text-white"
-                      >
-                        LOGIN
-                      </button>
-                    </div>
-                    <div>
-                      <Link to="/reset-password">
-                        <button className="text-mute">FORGOT PASSWORD ?</button>
-                      </Link>
+                    <div className="flex items-center gap-10">
+                      <div>
+                        <button
+                          type="submit"
+                          className="px-8 py-2.5 rounded-full bg-primary text-white"
+                        >
+                          LOGIN
+                        </button>
+                      </div>
+                      <div>
+                        <Link to="/reset-password">
+                          <button className="text-mute">
+                            FORGOT PASSWORD ?
+                          </button>
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
