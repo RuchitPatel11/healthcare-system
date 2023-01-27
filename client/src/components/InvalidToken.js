63dbf85669fb99af8e88a497fac02824650fa8d4
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2023-01-27 18:38:21 +0530

    Jan27

--- client/src/components/InvalidToken.js
+++ client/src/components/InvalidToken.js
@@ -4,7 +4,7 @@ const InvalidToken = () => {
   return (
     <div className="flex items-center justify-center gap-2 text-5xl text-yellow-500 h-96">
       <span className="fa-solid fa-triangle-exclamation"></span>
-      <h1>Invalid Token</h1>
+      <h1>This Link does not exist's anymore</h1>
     </div>
   );
 };
