d4138bf0e30fab112bd6f3b296fcfcfaa5274723
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2023-02-06 18:59:31 +0530

    Feb06

--- client/src/components/Dashboard/LeftBar.js
+++ client/src/components/Dashboard/LeftBar.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useAuth } from "../../hooks/useAuth";
 import APIButton from "./APIButton";
 
 const LeftBar = () => {
+  const { auth } = useAuth();
   return (
     <div className="flex flex-col gap-1">
       <APIButton to={"Doctor"} name="Doctors" icon="fa-solid fa-user-doctor" />
-      <APIButton to={"Patient"} name="Patients" icon="fa-solid fa-bed-pulse" />
+      {["Doctor", "Admin"].includes(auth.user.role) && (
+        <APIButton
+          to={"Patient"}
+          name="Patients"
+          icon="fa-solid fa-bed-pulse"
+        />
+      )}
       <APIButton
         to={"Nurse"}
         name="Nursing Staff"
@@ -18,7 +26,6 @@ const LeftBar = () => {
       />
       <APIButton to={"Medicine"} name="Medicines" icon="fa-solid fa-capsules" />
       <APIButton to={"Disease"} name="Diseases" icon="fa-solid fa-viruses" />
-      {/* <APIButton to={"Employee"} name="Employees" icon="fa-solid fa-user" /> */}
     </div>
   );
 };
