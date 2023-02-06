4b8b5c463df95d5c44771879e0ab726d7ceeffda
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2023-02-06 10:57:46 +0530

    Feb06

--- client/src/components/Dashboard/AddUserModal.js
+++ client/src/components/Dashboard/AddUserModal.js
@@ -1,81 +1,40 @@
-import { joiResolver } from "@hookform/resolvers/joi";
 import axios from "axios";
-import Joi from "joi";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import Loading from "../Loading";
 import PrimaryHeading from "../PrimaryHeading";
-import FormField from "../Register/FormField";
 import { useAuth } from "../../hooks/useAuth";
 import PrimaryButton from "../Header/PrimaryButton";
 import { useParams } from "react-router-dom";
-const registerSchema = Joi.object({
-  email: Joi.string()
-    .email({ tlds: { allow: false } })
-    .messages({
-      "string.email": "Enter valid email address",
-      "string.empty": "Email is required",
-    }),
-  first_name: Joi.string()
-    .required()
-    .trim()
-    .min(3)
-    .max(15)
-    .messages({ "string.empty": "First Name is required" })
-    .label("First Name"),
-  last_name: Joi.string()
-    .required()
-    .trim()
-    .min(3)
-    .max(15)
-    .messages({ "string.empty": "Last Name is required" })
-    .label("Last Name"),
-  gender: Joi.string().valid("Male", "Female").required().messages({
-    "any.only": "Gender is Required",
-  }),
-  phoneNo: Joi.string()
-    .pattern(/^[6-9]{1}\d{9}$/)
-    .required()
-    .messages({
-      "string.pattern.base": "Enter valid contact number",
-      "string.empty": "Contact No is required",
-    }),
-});
+import NewUserForm from "./NewUserForm";
+
 const AddUserModal = ({ onAdd }) => {
   const [state, setState] = useState("idle");
   const [showModal, setShowModal] = useState(false);
   const { auth } = useAuth();
   const { role } = useParams();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: joiResolver(registerSchema),
-    mode: "all",
-  });
 
-  const onSubmit = async (data) => {
-    try {
-      setState("submitting");
-      const res = await axios.post(
-        `${process.env.REACT_APP_PATH_NAME}/user/create-user`,
-        { ...data, role },
-        {
-          headers: { authorization: auth.token },
+  const onSubmit = (reset) => {
+    return async (data) => {
+      try {
+        setState("submitting");
+        const res = await axios.post(
+          `${process.env.REACT_APP_PATH_NAME}/user/create-user`,
+          { ...data, role },
+          {
+            headers: { authorization: auth.token },
+          }
+        );
+        if (res.status === 200) {
+          reset();
+          setState("success");
+          onAdd();
         }
-      );
-      if (res.status === 200) {
-        reset();
-        setState("success");
-        onAdd();
+      } catch (error) {
+        console.error(error);
+        alert(error.response.data);
+        setState("error");
       }
-    } catch (error) {
-      console.error(error);
-      alert(error.response.data);
-      setState("error");
-    }
+    };
   };
 
   return (
@@ -114,105 +73,10 @@ const AddUserModal = ({ onAdd }) => {
                   </div>
                 )}
                 {state === "idle" && (
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex flex-col gap-5 px-10">
-                      <div className="p-2">
-                        <PrimaryHeading name={`Add ${role}`} />
-                      </div>
-                      <FormField
-                        type="text"
-                        error={errors.email}
-                        register={register("email")}
-                        placeholder="you@gmail.com"
-                        name="email"
-                        icon="fa-solid fa-envelope"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.first_name}
-                        register={register("first_name")}
-                        placeholder="First Name"
-                        name="first_name"
-                        icon="fa-solid fa-user"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.last_name}
-                        register={register("last_name")}
-                        placeholder="Last Name"
-                        name="last_name"
-                        icon="fa-solid fa-user"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.phoneNo}
-                        register={register("phoneNo")}
-                        placeholder="Contact No"
-                        name="phoneNo"
-                        icon="fa-solid fa-phone"
-                      />
-                      <div>
-                        <div className="relative flex gap-5 p-4 mt-2 border">
-                          <label
-                            htmlFor="gender"
-                            className="absolute text-lg bg-white -top-4 left-3 text-extrabold text-primary"
-                          >
-                            Gender:
-                          </label>
-                          <div className="flex items-center">
-                            <input
-                              id="Male"
-                              type="radio"
-                              value="Male"
-                              name="gender"
-                              {...register("gender")}
-                              className="w-4 h-4 accent-primary"
-                            />
-                            <label
-                              htmlFor="Male"
-                              className="ml-2 font-medium text-gray-600 text-md dark:text-gray-300"
-                            >
-                              Male
-                            </label>
-                          </div>
-                          <div className="flex items-center">
-                            <input
-                              id="Female"
-                              type="radio"
-                              value="Female"
-                              {...register("gender")}
-                              name="gender"
-                              className="w-4 h-4 accent-primary"
-                            />
-                            <label
-                              htmlFor="Female"
-                              className="ml-2 font-medium text-gray-600 text-md dark:text-gray-300"
-                            >
-                              Female
-                            </label>
-                          </div>
-                        </div>
-                        {errors.gender && (
-                          <span className="flex items-center gap-2 m-1 text-red-600">
-                            <span className="fa-solid fa-circle-exclamation"></span>
-                            {errors.gender.message}
-                          </span>
-                        )}
-                      </div>
-                      <div className="my-3">
-                        <button
-                          type="submit"
-                          className="px-8 py-2.5 rounded-full bg-primary text-white"
-                        >
-                          {state === "submitting" ? (
-                            <Loading size={"text-lg"} name="Loading..." />
-                          ) : (
-                            <span>SUBMIT</span>
-                          )}
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                  <div className="flex flex-col gap-7">
+                    <PrimaryHeading name={`Add New ${role}`} />
+                    <NewUserForm onSubmit={onSubmit} />
+                  </div>
                 )}
                 {state === "error" && (
                   <div className="flex justify-center gap-2 py-16 text-3xl font-medium text-red-700 first-line:items-center px-28">
--- client/src/components/Dashboard/EditUserModal.js
+++ client/src/components/Dashboard/EditUserModal.js
@@ -1,84 +1,34 @@
 import React, { useState } from "react";
-import Joi from "joi";
-import { useForm } from "react-hook-form";
-import { joiResolver } from "@hookform/resolvers/joi";
-import FormField from "../Register/FormField";
 import axios from "axios";
 import { useAuth } from "../../hooks/useAuth";
 import Loading from "../Loading";
 import PrimaryHeading from "../PrimaryHeading";
-
-const updateUserSchema = Joi.object({
-  _id: Joi.string().hex().length(24).required(),
-  email: Joi.string()
-    .email({ tlds: { allow: false } })
-    .messages({
-      "string.email": "Enter valid email address",
-      "string.empty": "Email is required",
-    }),
-  first_name: Joi.string()
-    .required()
-    .trim()
-    .min(3)
-    .max(15)
-    .messages({ "string.empty": "First Name is required" })
-    .label("First Name"),
-  last_name: Joi.string()
-    .required()
-    .trim()
-    .min(3)
-    .max(15)
-    .messages({ "string.empty": "Last Name is required" })
-    .label("Last Name"),
-  gender: Joi.string().valid("Male", "Female").required().messages({
-    "any.only": "Gender is Required",
-  }),
-  phoneNo: Joi.string()
-    .pattern(/^[6-9]{1}\d{9}$/)
-    .required()
-    .messages({
-      "string.pattern.base": "Enter valid contact number",
-      "string.empty": "Contact No is required",
-    }),
-  role: Joi.string()
-    .valid("Doctor", "Pharmacist", "Nurse", "Admin")
-    .required()
-    .messages({
-      "any.only": "Role is required",
-    }),
-});
+import AddUserForm from "./NewUserForm";
 
 const EditUserModal = ({ details, onUpdate }) => {
   const [showModal, setShowModal] = useState(false);
   const [state, setState] = useState("idle");
   const { auth } = useAuth();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: joiResolver(updateUserSchema),
-    mode: "all",
-  });
 
-  const updateUsers = async (data) => {
-    try {
-      setState("submitting");
-      const id = data._id;
-      delete data._id;
-      await axios.put(
-        `http://localhost:4000/user/update/${id}`,
-        { ...data },
-        {
-          headers: { authorization: auth.token },
-        }
-      );
-      setState("success");
-      onUpdate();
-    } catch (error) {
-      console.log(error);
-      setState("error");
-    }
+  const updateUsers = () => {
+    return async (data) => {
+      try {
+        setState("submitting");
+
+        await axios.put(
+          `${process.env.REACT_APP_PATH_NAME}/user/update/${details._id}`,
+          { ...data },
+          {
+            headers: { authorization: auth.token },
+          }
+        );
+        setState("success");
+        onUpdate();
+      } catch (error) {
+        console.log(error);
+        setState("error");
+      }
+    };
   };
 
   return (
@@ -115,145 +65,10 @@ const EditUserModal = ({ details, onUpdate }) => {
                   </div>
                 )}
                 {state === "idle" && (
-                  <form onSubmit={handleSubmit(updateUsers)}>
-                    <div className="flex flex-col gap-4 px-10">
-                      <PrimaryHeading name={`Update ${details.role}`} />
-                      <div>
-                        <input
-                          type="hidden"
-                          defaultValue={details._id}
-                          {...register("_id")}
-                        />
-                        <div>
-                          <label htmlFor="role" className="text-primary">
-                            Choose Account Type:
-                          </label>
-                          <select
-                            id="role"
-                            className="border w-full p-2.5 bg-white"
-                            defaultValue={details.role}
-                            {...register("role")}
-                          >
-                            <option value="Doctor">Doctor</option>
-                            <option value="Nurse">Nurse</option>
-                            <option value="Pharmacist">Pharmacist</option>
-                          </select>
-                        </div>
-                        {errors.role && (
-                          <span className="flex items-center gap-2 m-1 text-red-600">
-                            <span className="fa-solid fa-circle-exclamation"></span>
-                            {errors.role.message}
-                          </span>
-                        )}
-                      </div>
-                      <FormField
-                        type="text"
-                        error={errors.email}
-                        defaultValue={details.email}
-                        register={register("email")}
-                        placeholder="you@gmail.com"
-                        name="email"
-                        icon="fa-solid fa-envelope"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.first_name}
-                        defaultValue={details.first_name}
-                        register={register("first_name")}
-                        placeholder="First Name"
-                        name="first_name"
-                        icon="fa-solid fa-user"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.last_name}
-                        defaultValue={details.last_name}
-                        register={register("last_name")}
-                        placeholder="Last Name"
-                        name="last_name"
-                        icon="fa-solid fa-user"
-                      />
-                      <FormField
-                        type="text"
-                        error={errors.phoneNo}
-                        defaultValue={details.phoneNo}
-                        register={register("phoneNo")}
-                        placeholder="Contact No"
-                        name="phoneNo"
-                        icon="fa-solid fa-phone"
-                      />
-                      <div>
-                        <div className="relative flex gap-5 p-4 mt-2 border">
-                          <label
-                            htmlFor="gender"
-                            className="absolute text-lg bg-white -top-4 left-3 text-extrabold text-primary"
-                          >
-                            Gender:
-                          </label>
-                          <div className="flex items-center">
-                            <input
-                              id="Male"
-                              type="radio"
-                              value="Male"
-                              name="gender"
-                              defaultChecked={details.gender === "Male"}
-                              {...register("gender")}
-                              className="w-4 h-4 accent-primary"
-                            />
-                            <label
-                              htmlFor="Male"
-                              className="ml-2 font-medium text-gray-600 text-md dark:text-gray-300"
-                            >
-                              Male
-                            </label>
-                          </div>
-                          <div className="flex items-center">
-                            <input
-                              id="Female"
-                              type="radio"
-                              value="Female"
-                              defaultChecked={details.gender === "Female"}
-                              {...register("gender")}
-                              name="gender"
-                              className="w-4 h-4 accent-primary"
-                            />
-                            <label
-                              htmlFor="Female"
-                              className="ml-2 font-medium text-gray-600 text-md dark:text-gray-300"
-                            >
-                              Female
-                            </label>
-                          </div>
-                        </div>
-                        {errors.gender && (
-                          <span className="flex items-center gap-2 m-1 text-red-600">
-                            <span className="fa-solid fa-circle-exclamation"></span>
-                            {errors.gender.message}
-                          </span>
-                        )}
-                      </div>
-                      <div className="flex gap-5 my-5">
-                        <div>
-                          <button
-                            className="px-6 py-2 text-sm font-bold text-white rounded-md bg-secondary"
-                            type="submit"
-                            // onClick={(e) => updateUsers(details._id)}
-                          >
-                            Save
-                          </button>
-                        </div>
-                        <div>
-                          <button
-                            className="px-6 py-2 text-sm font-bold text-white bg-red-600 rounded-md"
-                            type="button"
-                            onClick={() => setShowModal(false)}
-                          >
-                            Cancel
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
+                  <div className="flex flex-col gap-7">
+                    <PrimaryHeading name={`Edit ${details.role}`} />
+                    <AddUserForm details={details} onSubmit={updateUsers} />
+                  </div>
                 )}
                 {state === "error" && (
                   <div className="flex justify-center gap-2 py-16 text-3xl font-medium text-red-700 first-line:items-center px-28">
